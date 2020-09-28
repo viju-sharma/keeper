e17205883adb80dd151e5eb1502122bed59e66b4
vs4711 <vijendersharma47@gmail.com>
2020-09-28 12:38:17 +0530

    Updates

---
+++ precache-manifest.20ddee2b01923d8599aee2c4de4c38e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba5017682f1741e3367615dd7054f2b5",
+    "url": "/keeper/index.html"
+  },
+  {
+    "revision": "0b1e90e27334848b2d38",
+    "url": "/keeper/static/js/2.90f3b997.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/keeper/static/js/2.90f3b997.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7c78947349233719ab7",
+    "url": "/keeper/static/js/main.065f8737.chunk.js"
+  },
+  {
+    "revision": "b0f3c69d11e458d5f477",
+    "url": "/keeper/static/js/runtime-main.3e2bed01.js"
+  }
+]);
